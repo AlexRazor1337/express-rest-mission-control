b625aae1c668df2d02f0540c7ae778e8bef2f512
AlexRazor <alexrazor1337@gmail.com>
2023-09-05 23:20:42 +0300

    Change DB env variable

--- backend/src/services/mongo.js
+++ backend/src/services/mongo.js
@@ -4,7 +4,7 @@ dotenv.config()
 import mongoose from 'mongoose';
 
 export const mongoConnect = async () => {
-    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 }
 
 export const mongoDisconnect = async () => {
